887d1d622d0ff6e399fe9267b641215ba9297b7a
ArnoBunckens <arno.bunckens@student.ucll.be>
2020-05-28 18:54:36 +0200

    test

--- src/test/java/ipminorpe/demo/DemoTaskListServiceImpTest.java
+++ src/test/java/ipminorpe/demo/DemoTaskListServiceImpTest.java
@@ -8,6 +8,8 @@ import ipminorpe.demo.service.TaskListServiceImp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,6 +19,20 @@ public class DemoTaskListServiceImpTest {
     @Autowired
     public TaskListService taskListService;
 
-    
+    TaskListService testTastService;
+    TaskDTO dto = new TaskDTO();
 
+    @BeforeTestClass
+    public void setup() {
+        dto.setNaam("Fantastisch");
+        dto.setDateTime(LocalDateTime.of(2020,12,31,23,59));
+        dto.setDescription("Niks werkt");
+        testTastService.addTask(dto);
+    }
+
+    @Test
+    public void testGetAll(){
+
+    }
 }
+
